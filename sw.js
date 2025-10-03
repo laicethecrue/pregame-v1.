2519746fbcfed653510630551cdbdb2e57b601ba
laicethecrue <laice.ndombi@gmail.com>
2025-10-03 18:37:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
 
+const CACHE = "pregame-v1";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./pregames.json"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
